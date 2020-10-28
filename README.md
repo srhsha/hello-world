b3f604338e7ecb21efd81d0d1a00dbac7e440176
srhsha <programmer00@naver.com>
2020-10-28 17:45:43 +0900

    hello upload

---
+++ README.md
@@ -0,0 +1,3 @@
+#HELLO-PARKS
+no memo
+<br>
---
+++ index.js
@@ -0,0 +1,12 @@
+
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world2')
+  });
+    var port = 3000;
+
+    app.listen(port, function(){
+        console.log('server on! http://localhost:' + port);
+    });
