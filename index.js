290385072224816874c9d8d585559a898331f78a
srhsha <programmer00@naver.com>
2020-10-29 15:55:37 +0900

    board first commit

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-
-var express = require("express");
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+//var methodOverride = require('method-override');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send('hello world2')
-  });
-    var port = 3000;
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
 
-    app.listen(port, function(){
-        console.log('server on! http://localhost:' + port);
-    });
+// Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+//app.use(methodOverride('_method'))
+
+// Routes
+app.use('/', require('./routes/home'));
+app.use('/contacts', require('./routes/contacts'));
+
+// Port setting
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
